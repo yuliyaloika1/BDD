215aa9fc95116ba34f523b574bb88200d22954ab
Yuliya Loika <yuliya_loika@epam.com>
2021-08-02 22:21:35 +0300

    commit

---
+++ test/config/protractor_config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+exports.config = {
+ allScriptsTimeout: 60000,
+ getPageTimeout: 60000,
+ specs: [path.resolve('./test/features/*.feature')],
+ framework: 'custom',
+ frameworkPath: require.resolve('protractor-cucumber-framework'),
+ capabilities: {
+     browserName: 'chrome',
+     chromeOptions: {
+         args: ['--no-sandbox', '--window-size=1920,1080']
+     },
+ },
+ disableChecks: true, 
+ directConnection: true,
+ cucumberOpts: {
+     require: [path.resolve('./test/step_definitions/**/*.js')],
+     ignoreUncaughtExceptions: true,
+     format: ['json:./reports/report.json'],
+     tags: '@smoke'
+ }
+};
