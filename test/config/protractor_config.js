245b33a150499824628d419d784cea0f1904a44c
Yuliya Loika <yuliya_loika@epam.com>
2021-08-25 00:17:12 +0300

    commit

--- test/config/protractor_config.js
+++ test/config/protractor_config.js
@@ -1,4 +1,14 @@
+const { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } = require('constants');
 const path = require('path');
+const yargs = require('yargs').argv;
+const reporter = require('cucumber-html-reporter');
+
+const reportOptions = {
+    theme: 'bootstrap', 
+    jsonFile: path.join(__dirname, '../reports/report.json'),
+    output: path.join(__dirname, '../reports/cucumber-report.html'),
+    reportSuitesAsScenarios: true
+};
 
 exports.config = {
  allScriptsTimeout: 60000,
@@ -7,6 +17,8 @@ exports.config = {
  framework: 'custom',
  frameworkPath: require.resolve('protractor-cucumber-framework'),
  capabilities: {
+     shardTestFiles: yargs.instances > 1,
+     maxInstances: yargs.instances || 1,
      browserName: 'chrome',
      chromeOptions: {
          args: ['--no-sandbox', '--window-size=1920,1080']
@@ -17,7 +29,13 @@ exports.config = {
  cucumberOpts: {
      require: [path.resolve('./test/step_definitions/**/*.js')],
      ignoreUncaughtExceptions: true,
-     format: ['json:./test/reports/report.json'],
-     tags: '@smoke'
+     format: ['json:./test/reports/report.json', './node_modules/cucumber-pretty'],
+     tags: yargs.tags || '@smoke'
+ }
+ onPrepare: () => {
+     return browser.waitForAngularEnabled(false);
+ },
+ afterLaunch: () => {
+     return reporter.generate(reportOptions)
  }
 };
